9f69005627159e2fc2118364b28aea6384b0854c
RoRobertGrgac <robert.grgac2@gmail.com>
2025-01-12 14:33:30 +0100

    CNN results

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from Utils.config import ENV_CONFIG, AGENT_CONFIG, DEVICE, LOG_PATH
+from Utils.analysis import plot_scores_from_log
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'rl-agents')))
+from rl_agents.agents.common.factory import load_environment, load_agent
+from scripts.experiments import evaluate
+
+if __name__ == "__main__":
+
+    print("---------- Executing main.py ----------")
+
+    env = load_environment(ENV_CONFIG)
+    print("Environment loaded")
+
+    agent = load_agent(AGENT_CONFIG, env)
+    print("Agent loaded")
+
+    print("Device: ", DEVICE)
+
+    print("Evaluating agent...")
+    evaluate(ENV_CONFIG, AGENT_CONFIG, {'--train': True, '--episodes': 10, '--recover': False, '--no-display': True, '--seed': 10, '--verbose': True, '--name-from-config': False, '--recover-from': False})
+    print("Agent evaluated")
+
+    #print("Plotting scores...")
+    #plot_scores_from_log(LOG_PATH)
+
+    print("---------- End of main.py ----------")
+
--- Utils/config.py
+++ Utils/config.py
@@ -15,7 +15,7 @@ import torch
 # -----------------------------------------
 
 LOG_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Logs")
-ENV_CONFIG = 'Utils\Enviroments\env_obs_convolution.json'  
+ENV_CONFIG = 'Utils/Enviroments/env_obs_convolution.json'  
 AGENT_CONFIG = 'Utils/Architectures/convolutionNN.json'
 LOG_PATH = 'out/HighwayEnv/DQNAgent/run_20250110-180255_21008/logging.1179206764352.21008.log'  # Replace with the actual path to your log file
 
